ac25225fe83848f6040db095de3b8d19c48acf1c
Dmitry <dmitry27zh@gmail.com>
2023-09-25 19:44:08 +0300

    Add favorites private route

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
 import SigninScreen from '../signin-screen/signin-screen';
 import FavoritesScreen from '../favorites-screen/favorites-screen';
 import RoomScreen from '../room-screen/room-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   placesCount: number;
@@ -16,7 +17,7 @@ function App({placesCount}: AppProps): JSX.Element {
       <Routes>
         <Route path={AppRoute.Main} element={<MainScreen placesCount={placesCount} />} />
         <Route path={AppRoute.Signin} element={<SigninScreen />}/>
-        <Route path={AppRoute.Favorites} element={<FavoritesScreen />}/>
+        <Route path={AppRoute.Favorites} element={<PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}><FavoritesScreen /></PrivateRoute>}/>
         <Route path={AppRoute.Room} element={<RoomScreen />}/>
         <Route path='*' element={<NotFoundScreen />}/>
       </Routes>
