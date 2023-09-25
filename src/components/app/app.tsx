0412a0d0a96629e1a35b72c823f6f46dfb2d1355
Dmitry <dmitry27zh@gmail.com>
2023-09-25 19:25:47 +0300

    Add Routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,25 @@
+import { AppRoute } from '../../const';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import SigninScreen from '../signin-screen/signin-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import RoomScreen from '../room-screen/room-screen';
 
 type AppProps = {
   placesCount: number;
 }
 
 function App({placesCount}: AppProps): JSX.Element {
-  return <MainScreen placesCount={placesCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainScreen placesCount={placesCount} />} />
+        <Route path={AppRoute.Signin} element={<SigninScreen />}/>
+        <Route path={AppRoute.Favorites} element={<FavoritesScreen />}/>
+        <Route path={AppRoute.Room} element={<RoomScreen />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/const.tsx
+++ src/const.tsx
@@ -1,3 +1,10 @@
 export const Setting = {
   PLACES_COUNT: 3,
 };
+
+export enum AppRoute {
+  Main = '/',
+  Signin = '/login',
+  Favorites = '/favorites',
+  Room = '/offer/:id'
+}
