438858c7571714eb7974e792d76a957ec0949f9c
Dmitry <dmitry27zh@gmail.com>
2023-09-25 19:06:36 +0300

    Create const module

---
+++ src/const.tsx
@@ -0,0 +1,3 @@
+export const Setting = {
+  PLACES_COUNT: 3,
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
-const Setting = {
-  PLACES_COUNT: 3,
-};
+import { Setting } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
