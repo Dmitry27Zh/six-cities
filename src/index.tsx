f9a823d703d99f2eb07a6be52cc9324fe1a1d669
Dmitry <dmitry27zh@gmail.com>
2023-09-27 12:11:11 +0300

    Pass data in App

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import { Setting } from './const';
+import { offers } from './mock/offers';
+import { reviews } from './mock/reviews';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -9,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App placesCount={Setting.PLACES_COUNT}/>
+    <App offers={offers} reviews={reviews}/>
   </React.StrictMode>,
 );
