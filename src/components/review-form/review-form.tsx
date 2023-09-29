1448629a1e7f5a73a4b9cec2b614e2ee92e915fc
Dmitry <dmitry27zh@gmail.com>
2023-09-29 10:59:05 +0300

    Control ReviewForm

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -1,49 +1,76 @@
+import { ChangeEvent, useState } from 'react';
+import { FormDataState } from '../../types/common';
+
 function ReviewForm():JSX.Element {
+  const [data, setData] = useState({
+    rating: '',
+    review: '',
+  } as FormDataState);
+  const onChange = ({target}: ChangeEvent) => {
+    const {name, value} = target as HTMLInputElement;
+    setData((prevData) => ({...prevData, [name]: value}));
+  };
+
   return (
     <form className="reviews__form form">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
+        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"
+          checked={data.rating === '5'}
+          onChange={onChange}
+        />
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
+        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"
+          checked={data.rating === '4'}
+          onChange={onChange}
+        />
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
+        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"
+          checked={data.rating === '3'}
+          onChange={onChange}
+        />
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
+        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"
+          checked={data.rating === '2'}
+          onChange={onChange}
+        />
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
+        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"
+          checked={data.rating === '1'}
+          onChange={onChange}
+        />
         <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
       </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={data.review} onChange={onChange}></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={false}>Submit</button>
       </div>
     </form>
   );
