fc7032818f61b212669ad776977b46274dc9df0a
Dmitry <dmitry27zh@gmail.com>
2023-10-17 18:25:46 +0300

    Create actions

--- src/store/city.ts
+++ src/store/city.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { City } from '../types/data';
 
 const initialState: City = {
@@ -13,7 +13,32 @@ const initialState: City = {
 export const citySlice = createSlice({
   name: 'city',
   initialState,
-  reducers: {}
+  reducers: {
+    change: (state, action: PayloadAction<string>) => {
+      const cityName = action.payload;
+      const city = [{
+        location: {
+          latitude: 52.3909553943508,
+          longitude: 4.85309666406198,
+          zoom: 10,
+        },
+        name: 'Amsterdam',
+      }, {
+        location: {
+          latitude: 50.935173,
+          longitude: 6.953101,
+          zoom: 10
+        },
+        name: 'Cologne'
+      }].find(({name}) => name === cityName);
+
+      if (city) {
+        Object.assign(state, city);
+      }
+    }
+  }
 });
+
+export const {change: changeCity} = citySlice.actions;
 
 export default citySlice.reducer;
--- src/store/offers.ts
+++ src/store/offers.ts
@@ -147,7 +147,48 @@ const initialState: Offers = [
 export const offersSlice = createSlice({
   name: 'offers',
   initialState,
-  reducers: {}
+  reducers: {
+    set: (state) => {
+      state.length = 0;
+      state.push({
+        bedrooms: 3,
+        city: {
+          location: {
+            latitude: 50.935173,
+            longitude: 6.953101,
+            zoom: 10
+          },
+          name: 'Cologne'
+        },
+        description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+        goods: ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine',
+          'Dishwasher'],
+        host: {
+          avatarUrl: 'img/1.png',
+          id: 3,
+          isPro: true,
+          name: 'Angelina'
+        },
+        id: 4,
+        images: ['img/1.png', 'img/2.png'],
+        isFavorite: true,
+        isPremium: false,
+        location: {
+          latitude: 50.935173,
+          longitude: 6.953101,
+          zoom: 8
+        },
+        maxAdults: 4,
+        previewImage: 'img/1.png',
+        price: 120,
+        rating: 1,
+        title: 'Beautiful & luxurious studio at great location',
+        type: 'apartment'
+      });
+    }
+  }
 });
+
+export const {set: setOffers} = offersSlice.actions;
 
 export default offersSlice.reducer;
